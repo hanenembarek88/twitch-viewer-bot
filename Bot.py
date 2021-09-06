67d4db189cb625af50f4871b107b42fd188a20f4
bishwas-py <bishwasbh@gmail.com>
2021-09-06 22:09:03 +0545

    Truth

--- Bot.py
+++ Bot.py
@@ -22,7 +22,7 @@ def get_options():
 def get_driver(options, proxy=""):
     options.add_argument(f"--proxy-server={proxy}")
     
-    options.add_argument(f"--user-data-dir=profile/{proxy}")
+    options.add_argument(f"--user-data-dir=profile/{proxy.replace('.','_').replace(':','-')}")
     driver = webdriver.Chrome(options=options)
     driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
     "source":
